78b056ce793fa7f31a514d49c03b5ab4f0f3ab36
ivannov <ivan_st_ivanov@yahoo.com>
2016-10-30 23:14:09 +0200

    Complete the content resources

--- sources/content/src/main/java/bg/jug/magman/resources/ContentResource.java
+++ sources/content/src/main/java/bg/jug/magman/resources/ContentResource.java
@@ -16,28 +16,59 @@
 package bg.jug.magman.resources;
 
 import bg.jug.magman.domain.Article;
+import bg.jug.magman.domain.Comment;
 import bg.jug.magman.persistence.ArticleDAO;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.List;
 
 @Path("/")
 @Produces("application/json")
-@Consumes("application/json")
 @RequestScoped
 public class ContentResource {
-
 
     @Inject
     private ArticleDAO articleDAO;
 
+    @GET
+    public Response getArticles() {
+        List<Article> articles = articleDAO.getAllArticles();
+        JsonArray articlesArray = buildArticlesJson(articles);
+        return Response.ok(articlesArray).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response findArticleById(@PathParam("id") final Long articleId) {
+        return articleDAO.findArticleById(articleId)
+                .map(article -> Response.ok(article ).build())
+                .orElse(Response.status(Response.Status.NOT_FOUND).build());
+    }
+
+    @GET
+    @Path("/{authorName}")
+    public Response findArticlesByAuthor(@PathParam("authorName") String authorName) {
+        List<Article> articles = articleDAO.findArticlesByAuthor(authorName);
+        JsonArray articlesArray = buildArticlesJson(articles);
+        return Response.ok(articlesArray).build();
+
+    }
+
+    private JsonArray buildArticlesJson(List<Article> articles) {
+        JsonArrayBuilder articlesArrayBuilder = Json.createArrayBuilder();
+        articles.forEach(article -> articlesArrayBuilder.add(article.toJson()));
+        return articlesArrayBuilder.build();
+    }
+
     @POST
+    @Consumes("application/json")
     public Response addArticle(String articleJson) {
         final Article article = Article.fromJson(articleJson);
         final Article created = articleDAO.addArticle(article);
@@ -45,4 +76,30 @@ public class ContentResource {
                 .entity(created.toJson())
                 .build();
     }
+
+    @PUT
+    @Consumes("application/json")
+    public Response updateArticle(String articleJson) {
+        articleDAO.updateArticle(Article.fromJson(articleJson));
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteArticle(@PathParam("id") Long articleId) {
+        articleDAO.deleteArticle(articleId);
+        return Response.noContent().build();
+    }
+
+    @POST
+    @Path("/{id}/comment")
+    @Consumes("application/json")
+    public Response addCommentToArticle(@PathParam("id") Long articleId, String commentJson) {
+        Comment comment = Comment.fromJson(commentJson);
+        Comment created = articleDAO.addCommentToArticle(comment, articleId);
+        return Response.created(URI.create("/" + created.getId()))
+                .entity(created.toJson())
+                .build();
+    }
+
 }
